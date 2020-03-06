c83f2180092800a353507635260a3d74ddd60324
git <river6935@gmail.com>
2020-03-06 17:53:37 +0800

    a

---
+++ go.mod
@@ -0,0 +1,11 @@
+module microservice/part1/orders-srv
+
+go 1.14
+
+require ( 
+github.com/micro-in-cn/tutorials/microservice-in-micro v0.0.0-20200305151203-23ee9d0da0c2 // indirect
+github.com/microservice/part1/basic v0.0.0
+
+)
+
+replace github.com/microservice/part1/basic => /home/river/go/src/microservice/part1/basic
---
+++ main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"fmt"
+
+	"microservice/part1/orders-srv/handler"
+	"microservice/part1/orders-srv/model"
+	"microservice/part1/orders-srv/subscriber"
+
+	proto "microservice/part1/orders-srv/proto/orders"
+
+	"github.com/gappy023/basic"
+	"github.com/gappy023/basic/common"
+	"github.com/gappy023/basic/config"
+	"github.com/micro/cli/v2"
+	"github.com/micro/go-micro/v2"
+	log "github.com/micro/go-micro/v2/logger"
+	"github.com/micro/go-micro/v2/registry"
+	"github.com/micro/go-micro/v2/registry/etcd"
+)
+
+func main() {
+	// 初始化配置、数据库等信息
+	basic.Init()
+
+	// 使用etcd注册
+	micReg := etcd.NewRegistry(registryOptions)
+
+	// 新建服务
+	service := micro.NewService(
+		micro.Name("mu.micro.book.srv.orders"),
+		micro.Registry(micReg),
+		micro.Version("latest"),
+	)
+
+	// 服务初始化
+	service.Init(
+		micro.Action(func(c *cli.Context) error {
+			// 初始化模型层
+			model.Init()
+			// 初始化handler
+			handler.Init()
+			// 初始化sub
+			subscriber.Init()
+
+			return nil
+		}),
+	)
+
+	// 侦听订单支付消息
+	err := micro.RegisterSubscriber(common.TopicPaymentDone, service.Server(), subscriber.PayOrder)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// 注册服务
+	err = proto.RegisterOrdersHandler(service.Server(), new(handler.Orders))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// 启动服务
+	if err = service.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func registryOptions(ops *registry.Options) {
+	etcdCfg := config.GetEtcdConfig()
+	ops.Addrs = []string{fmt.Sprintf("%s:%d", etcdCfg.GetHost(), etcdCfg.GetPort())}
+}
